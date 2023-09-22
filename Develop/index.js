f8c1107fe15b657ea5ecc18323cd42e839ead2d2
keithprimas <136135449+keithprimas@users.noreply.github.com>
2023-09-22 10:51:32 -0400

    updated index,js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,47 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateREADME = require('./generateREADME');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+async function main() {
+    const userInput = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title:',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter the installation instructions',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter the usage information',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter the contribution guidelines',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Enter the test instructions',
+      },
+    
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function main() {}
 
 // Function call to initialize app
-init();
+main();
