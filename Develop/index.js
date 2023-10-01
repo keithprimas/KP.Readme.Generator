a8a1c5754d386759d78d6c0fe48598a6bdf65782
keithprimas <136135449+keithprimas@users.noreply.github.com>
2023-10-01 13:12:27 -0400

    updated json

--- Develop/index.js
+++ Develop/index.js
@@ -1,47 +1,93 @@
 const inquirer = require('inquirer');
 const generateREADME = require('./generateREADME');
+const writeREADMEToFile = require('./writeREADMEToFile'); // Import the writeREADMEToFile function
 
-// TODO: Create an array of questions for user input
 async function main() {
-    const userInput = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'title',
-        message: 'Enter your project title:',
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a description:',
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'Enter the installation instructions',
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter the usage information',
-      },
-      {
-        type: 'input',
-        name: 'contributing',
-        message: 'Enter the contribution guidelines',
-      },
-      {
-        type: 'input',
-        name: 'test',
-        message: 'Enter the test instructions',
-      },
-    
-    ]);
+  const userInput = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter your project title:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter the installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter the usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter the contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Enter the test instructions:',
+    },
+  ]);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  // Generate the README using the user input
+  const readmeContent = generateREADME(userInput);
 
-// TODO: Create a function to initialize app
-function main() {}
+  // Write the README content to a file
+  writeREADMEToFile(readmeContent); // Call the writeREADMEToFile function with the content
+}
 
 // Function call to initialize app
 main();
+
+
+// generateREADME.js
+
+function generateREADME(userInput) {
+  // Construct the README content based on userInput
+  const readmeContent = `
+  # ${userInput.title}
+
+  ## Description
+  ${userInput.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Installation
+  ${userInput.installation}
+
+  ## Usage
+  ${userInput.usage}
+
+  ## Contributing
+  ${userInput.contributing}
+
+  ## Test
+  ${userInput.test}
+
+  ## License
+  // Add the chosen license badge and notice
+
+  ## Questions
+  GitHub: [GitHub Profile](https://github.com/${userInput.githubUsername})
+  Email: ${userInput.email}
+  `;
+
+  // Write the content to a README.md file
+  // Use file system operations like fs.writeFileSync() to create the README file
+}
+
+module.exports = generateREADME;
+
